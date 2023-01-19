c989adb52b8a87e4f51e73a979bfcd468e9fba9c
jloriso <105647279+jloriso@users.noreply.github.com>
2023-01-18 21:18:39 -0600

    Add graphGenerator file

---
+++ graphGenerator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_vertex_name():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+def main():
+    num_vertices = 10
+    num_edges = 15
+    vertex_names = set()
+    for i in range(num_edges):
+        vertex1 = generate_vertex_name()
+        vertex2 = generate_vertex_name()
+        while vertex1 == vertex2:
+            vertex2 = generate_vertex_name()
+        weight = round(random.uniform(0, 100), 4)
+        print(f"{vertex1} {vertex2} {weight}")
+
+main()
