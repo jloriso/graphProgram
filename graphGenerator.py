0ab3c900f9c4fe725413f335898ebe0a5a5de649
jloriso <105647279+jloriso@users.noreply.github.com>
2023-01-19 20:52:08 -0600

    added random generation and fixed user input generation

--- graphGenerator.py
+++ graphGenerator.py
@@ -6,21 +6,28 @@ def generate_vertex_name():
 
 def main():
     num_vertices = 10
-    num_edges = 15
+    num_edges = verticesInput()
     vertex_names = set()
     for i in range(num_edges):
         vertex1 = generate_vertex_name()
         vertex2 = generate_vertex_name()
         while vertex1 == vertex2:
             vertex2 = generate_vertex_name()
-        weight = round(random.uniform(0, 100), 4)
+        weight = round(random.uniform(-100, 100), 4)
         print(f"{vertex1} {vertex2} {weight}")
 
 
 def verticesInput():
     while True:
         try:
-            n = int(input("Enter an integer: "))
+            n = input("Enter an integer or enter r for random: ")
+
+            if n == "r" or n == "R":
+                n = random.randint(1,50)
+                print(n)
+            else:
+                n = int(n)
+
             return n
         except ValueError:
             print("Invalid input. Please enter an integer.")
