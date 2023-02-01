a5068a64d55e815206b364f2925adf9bd99ebbc8
jloriso <105647279+jloriso@users.noreply.github.com>
2023-01-31 21:59:20 -0600

    take arguments from cmd line

--- graphGenerator.py
+++ graphGenerator.py
@@ -1,36 +1,32 @@
 import random
 import string
+import argparse
 
 def generate_vertex_name():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
 
 def main():
-    num_vertices = 10
-    num_edges = verticesInput()
+    parser = argparse.ArgumentParser(description="Generate a weighted graph")
+    parser.add_argument("--vertices", type=int, help="Number of vertices in the graph")
+    parser.add_argument("--connected", action="store_true", help="Generate a connected graph")
+    args = parser.parse_args()
+
+    num_vertices = args.vertices if args.vertices else random.randint(1, 20)
+    print(num_vertices)
+    is_connected = args.connected
+    generate(num_vertices, is_connected)
+
+
+def generate(num_vertices, is_connected):
     vertex_names = set()
-    for i in range(num_edges):
+    count = 1
+    for i in range(num_vertices):
         vertex1 = generate_vertex_name()
         vertex2 = generate_vertex_name()
         while vertex1 == vertex2:
             vertex2 = generate_vertex_name()
         weight = round(random.uniform(-100, 100), 4)
-        print(f"{vertex1} {vertex2} {weight}")
-
-
-def verticesInput():
-    while True:
-        try:
-            n = input("Enter an integer or enter r for random: ")
-
-            if n == "r" or n == "R":
-                n = random.randint(1,50)
-                print(n)
-            else:
-                n = int(n)
-
-            return n
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
-
+        print(count, f"{vertex1} {vertex2} {weight}")
+        count += 1
             
 main()
