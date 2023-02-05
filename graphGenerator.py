b2d7ada21236a80711aa07f13370b78ba06f6a10
jloriso <105647279+jloriso@users.noreply.github.com>
2023-02-05 17:28:52 -0600

    fixed output for connected and unconnected, generates connected sometimes when unspecified

--- graphGenerator.py
+++ graphGenerator.py
@@ -3,7 +3,7 @@ import string
 import argparse
 
 def generate_vertex_name():
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(2))
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a weighted graph")
@@ -17,6 +17,11 @@ def main():
     else:
         num_vertices = random.randint(1,25)
     is_connected = args.connected
+    if is_connected == False:
+        #randomly make a graph connected if not specified
+        if random.random() > 0.5:
+            is_connected = True
+            print("TRUE")
     generate(num_vertices, is_connected)
 
 
@@ -29,34 +34,30 @@ def generate(num_vertices, is_connected):
     #generate vertices
     nodes1 = []
     nodes2 = []
+    edges = []
     for i in range(num_vertices):
+        #generate vertices
         nodes1.append(generate_vertex_name())
         vertex2 = generate_vertex_name()
         #check if vertices are the same
         while nodes1[i] == vertex2:
             vertex2 = generate_vertex_name()
         nodes2.append(vertex2)
-
-    #create weights for nodes
-    edges = []
-    for i in range(num_vertices):
-        if is_connected:
-            edge = random.uniform(-99,99)
-            edge = roundEdge(edge)
-            #check if edge already exists
-            for ed in edges:
-                while ed == edge:
-                    edge = random.uniform(-99,99)
-                    edge = roundEdge(edge)
-            edges.append(edge)
+        
+        #create weights for nodes
+        edge = random.uniform(-99,99)
+        edge = roundEdge(edge)
+        #check if edge already exists
+        for ed in edges:
+            while ed == edge:
+                edge = random.uniform(-99,99)
+                edge = roundEdge(edge)
+        edges.append(edge)
 
     #print out
     count = 1
     for i in range(num_vertices):
-        if(is_connected):
-            print(count, f"{nodes1[i]} {nodes2[i]} {edges[i]}")
-        else:
-             print(count, f"{nodes1[i]} {nodes2[i]}")
+        print(count, f"{nodes1[i]} {nodes2[i]} {edges[i]}")
         count += 1
             
 main()
