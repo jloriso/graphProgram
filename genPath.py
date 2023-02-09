83f261a1caa6ec8be65c080e89b134f463f5ef83
jloriso <105647279+jloriso@users.noreply.github.com>
2023-02-08 23:06:03 -0600

    init genPath for part b, start parse input and main function, will use bfs for path finding

--- genPath.py
+++ genPath.py
@@ -1,29 +1,22 @@
+from collections import defaultdict, deque
 import sys
-import re
-from collections import defaultdict
 
 def parse_input():
     graph = defaultdict(list)
-    for i, line in enumerate(sys.stdin, 1):
-        edge = line.strip().split()
-        if len(edge) != 3:
-            print("Invalid input format at line", i)
-            sys.exit(1)
-        node1, node2, weight = edge
-        if not re.match("^[a-zA-Z0-9]+$", node1) or not re.match("^[a-zA-Z0-9]+$", node2):
-            print("Invalid node name at line", i)
-            sys.exit(1)
-        try:
-            weight = float(weight)
-        except ValueError:
-            print("Invalid weight at line", i)
-            sys.exit(1)
-        if node1 in graph and (node2, weight) in graph[node1]:
-            print("Duplicate edge at line", i, "first appeared at", graph[node1].index((node2, weight)) + 1)
-            sys.exit(1)
-        graph[node1].append((node2, weight))
-        graph[node2].append((node1, weight))
+    for line in sys.stdin:
+        src, dest, weight = line.strip().split()
+        graph[src].append((dest, weight))
+        graph[dest].append((src, weight))
     return graph
 
-def bfs(graph, start, end):
+def find_shortest_path(graph, start, end):
     pass
+
+def main():
+    start, end = sys.argv[1], sys.argv[2]
+    graph = parse_input()
+    path, length = find_shortest_path(graph, start, end)
+    print(f"Path: {path}")
+    print(f"Length: {length}")
+
+main()
