4506a985eb21cffe278ded6fec1996a2eed0066e
jloriso <105647279+jloriso@users.noreply.github.com>
2023-02-14 21:18:50 -0600

    Fixed input method, oo design

--- genPath.py
+++ genPath.py
@@ -1,49 +1,69 @@
 from collections import defaultdict, deque
 import sys
-import re
 
-def parse_input():
-    graph = defaultdict(list)
-    for i, line in enumerate(sys.stdin, 1):
-        edge = line.strip().split()
-        if len(edge) != 3:
-            print("Invalid input format at line", i)
+class Graph:
+    def __init__(self):
+        self.vertices = set()
+        self.edges = {}
+        
+    def add_edge(self, u, v, w):
+        self.vertices.add(u)
+        self.vertices.add(v)
+        if (u, v) in self.edges:
+            print(f"Duplicate edge ({u}, {v})")
             sys.exit(1)
-        node1, node2, weight = edge
-        if not re.match("^[a-zA-Z0-9]+$", node1) or not re.match("^[a-zA-Z0-9]+$", node2):
-            print("Invalid node name at line", i)
-            sys.exit(1)
-        try:
-            weight = float(weight)
-        except ValueError:
-            print("Invalid weight at line", i)
-            sys.exit(1)
-        if node1 in graph and (node2, weight) in graph[node1]:
-            print("Duplicate edge at line", i, "first appeared at", graph[node1].index((node2, weight)) + 1)
-            sys.exit(1)
-        graph[node1].append((node2, weight))
-        graph[node2].append((node1, weight))
-    return graph
+        self.edges[(u, v)] = w
 
-def find_shortest_path(graph, start, end):
-    queue = [(start, [start])]
-    visited = set()
-    while queue:
-        (vertex, path) = queue.pop(0)
-        if vertex in visited:
-            continue
-        visited.add(vertex)
-        for (neighbour, weight) in graph[vertex]:
-            if neighbour == end:
-                return path + [end]
-            queue.append((neighbour, path + [neighbour]))
-    return None
+    def bfs(self, start, end):
+        if start not in self.vertices:
+            print("Vertex ", start, " not in graph")
+            sys.exit(1)
+        if end not in self.vertices:
+            print("Vertex ", end, " not in graph")
+            sys.exit(1)
+
+        queue = [(start, [start])]
+        visited = set()
+        while queue:
+            (vertex, path) = queue.pop(0)
+            if vertex in visited:
+                continue
+            visited.add(vertex)
+            for (neighbour, weight) in self[vertex]:
+                if neighbour == end:
+                    return path + [end]
+                queue.append((neighbour, path + [neighbour]))
+        return None
 
 def main():
-    start, end = sys.argv[1], sys.argv[2]
-    graph = parse_input()
-    path, length = find_shortest_path(graph, start, end)
-    print(f"Path: {path}")
-    print(f"Length: {length}")
+    g = Graph()
+    for line in sys.stdin:
+        parts = line.strip().split()
+        if len(parts) != 3:
+            break
+        u, v, w = parts
+        if not all(c.isalnum() for c in u):
+            print(f"Invalid vertex name: {u}")
+            sys.exit(1)
+        if not all(c.isalnum() for c in v):
+            print(f"Invalid vertex name: {v}")
+            sys.exit(1)
+        try:
+            w = float(w)
+        except ValueError:
+            print(f"Invalid weight: {w}")
+            sys.exit(1)
+        g.add_edge(u,v,w)
+
+    if len(sys.argv) != 3:
+        if len(sys.argv) == 1 and sys.argv == "^D":
+            print("Stopping input")
+        else:
+            print("Usage: sp start end")
+            sys.exit(1)
+    start, end = sys.argv[1:3]
+    path, dist = g.bfs(start, end)
+    print(f"Path: {'-'.join(path)}")
+    print(f"Length: {dist}")
 
 main()
